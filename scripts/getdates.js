19592c9d9abdea1efcc5f500df6f8c25d34a7b7a
miquelyn10 <miquelyn10@yahoo.com>
2025-05-30 16:21:16 -0500

    restart

--- scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-
-// Populate the current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Populate the last modified date in the second paragraph
-document.getElementById("lastmodified").textContent = `Last Modified: ${document.lastModified}`;
